a47254f842c80ec5e1458d3a4286b2eb97865713
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2025-06-17 07:34:31 +0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -28,7 +28,7 @@ async def start(client, message):
     if not await db.is_user_exist(user.id):
         await db.add_user(user.id, user.first_name)
     reply_markup = InlineKeyboardMarkup(main_buttons)
-    jishubotz = await message.reply_sticker("CAACAgUAAxkBAAENy31nsAR9bgne2-FRSUQ9-zEy2MyixwACUBMAAuCPGFbErxOwKx4X-TYE)
+    jishubotz = await message.reply_sticker("CAACAgUAAxkBAAENy31nsAR9bgne2-FRSUQ9-zEy2MyixwACUBMAAuCPGFbErxOwKx4X-TYE")
     await asyncio.sleep(2)
     await jishubotz.delete()
     text=Translation.START_TXT.format(user.mention)
