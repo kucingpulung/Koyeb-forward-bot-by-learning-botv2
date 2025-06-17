3eb5541e42153c5dea65d14b9f5949e0497ba0d2
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2025-06-17 15:45:57 +0700

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,18 +1,14 @@
-# Webserver untuk Uptime Robot / Koyeb
-
 from aiohttp import web
 
-# Fungsi respons saat homepage diakses
 async def handle(request):
-    print("✅ Ping dari UptimeRobot diterima.")
+    print("✅ Ping dari UptimeRobot atau Koyeb diterima.")
     return web.Response(text="✅ Bot aktif dan berjalan!")
 
-# Fungsi utama webserver
 async def start_webserver():
     app = web.Application()
     app.add_routes([web.get("/", handle)])
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", 80)  # GANTI ke port 80 agar bisa diakses UptimeRobot
+    site = web.TCPSite(runner, host="0.0.0.0", port=80)
     await site.start()
     print("🌐 Web server aktif di http://localhost:80/")
