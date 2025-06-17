28e1e9c2615573d70c29fbeba9122d59849e7347
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2025-06-17 07:16:53 +0700

    Create webserver.py

---
+++ webserver.py
@@ -0,0 +1,13 @@
+from aiohttp import web
+
+async def handle(request):
+    return web.Response(text="Bot aktif!")
+
+async def start_webserver():
+    app = web.Application()
+    app.add_routes([web.get("/", handle)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, "0.0.0.0", 8080)
+    await site.start()
+    print("🌐 Web server aktif di http://localhost:8080/")
