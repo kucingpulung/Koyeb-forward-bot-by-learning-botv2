da388c48f67c179e51d82140719a14bfed1cf1b4
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2025-06-18 00:22:21 +0700

    Update database.py

--- database.py
+++ database.py
@@ -104,15 +104,15 @@ class Database:
             'button': None,
             'db_uri': None,
             'filters': {
-               'poll': True,
-               'text': True,
-               'audio': True,
-               'voice': True,
+               'poll': False,
+               'text': False,
+               'audio': False,
+               'voice': False,
                'video': True,
-               'photo': True,
-               'document': True,
-               'animation': True,
-               'sticker': True
+               'photo': False,
+               'document': False,
+               'animation': False,
+               'sticker': False
             }
         }
         user = await self.col.find_one({'id':int(id)})
